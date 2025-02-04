24ef62732bc8897b0be857173d3f35ebe8d30ce0
shou-317 <shou6130@gmail.com>
2025-02-04 16:32:57 +0900

    Refactor code by extracting methods for better readability

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,27 +2,35 @@
 require 'date'
 require 'optparse'
 
-option_parser = OptionParser.new
-params = {}
-option_parser.on('-m [VAL]', Integer) {|m| params[:month] = m}
-option_parser.on('-y [VAL]', Integer) {|m| params[:year] = m}
-option_parser.parse!(ARGV, into: params)
+def parse_arguments
+  option_parser = OptionParser.new
+  params = {}
+  option_parser.on('-m [VAL]', Integer) {|m| params[:month] = m}
+  option_parser.on('-y [VAL]', Integer) {|m| params[:year] = m}
+  option_parser.parse!(ARGV, into: params)
+  params[:year] ||= Date.today.year
+  params[:month] ||= (params[:year] == Date.today.year ? Date.today.month : 1)
+  generate_calendar(params)
+end
 
-params[:year] ||= Date.today.year
-params[:month] ||= (params[:year] == Date.today.year ? Date.today.month : 1)
+def generate_calendar(params)
+  first_day = Date.new(params[:year], params[:month], 1)
+  last_day = Date.new(params[:year], params[:month], -1)
+  days_in_month = last_day.day
+  leading_spaces = [' '] * first_day.wday
+  calendar_rows = (1..days_in_month)
+    .to_a
+    .unshift(*leading_spaces)
+    .each_slice(7)
+    .map {|week| week.map { |day| day.to_s.rjust(2) }.join(' ') }
+    .join("\n")
+  display_calendar(params, calendar_rows)
+end
 
-first_day = Date.new(params[:year], params[:month], 1)
-last_day = Date.new(params[:year], params[:month], -1)
+def display_calendar(params, calendar_rows)
+  puts "      #{params[:month]}月 #{params[:year]}"
+  puts %w(日 月 火 水 木 金 土).join(' ') + "\n"
+  puts calendar_rows
+end
 
-puts "      #{first_day.month}月 #{first_day.year}"
-puts %w(日 月 火 水 木 金 土).join(' ') + "\n"
-
-days_in_month = last_day.day
-leading_spaces = [' '] * first_day.wday
-
-puts (1..days_in_month)
-  .to_a
-  .unshift(*leading_spaces)
-  .each_slice(7)
-  .map {|week| week.map { |day| day.to_s.rjust(2) }.join(' ') }
-  .join("\n")
+parse_arguments
