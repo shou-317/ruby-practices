0df84127ad4f8cf58448b226b1ee5166025dd320
shou-317 <shou6130@gmail.com>
2025-02-04 15:27:47 +0900

    Rename variables and clean up default values
    - Renamed variables for better readability
    - Removed unnecessary `.to_int` calls
    - Simplified default year/month handling for better clarity
    - Replaced `params` keys (`:m` → `:month`, `:y` → `:year`) for consistency

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,31 +2,27 @@
 require 'date'
 require 'optparse'
 
-opt = OptionParser.new
-date = {}
-opt.on('-m [VAL]', Integer)
-opt.on('-y [VAL]', Integer)
-opt.parse!(ARGV, into: date)
-date.fetch(:y) { date[:y] = Date.today.year}
-date.fetch(:m) do
-  if date[:y] == Date.today.year
-    date[:m] = Date.today.month
-  else
-    date[:m] = 1
-  end
-end
+option_parser = OptionParser.new
+params = {}
+option_parser.on('-m [VAL]', Integer) {|m| params[:month] = m}
+option_parser.on('-y [VAL]', Integer) {|m| params[:year] = m}
+option_parser.parse!(ARGV, into: params)
 
-first = Date.new(date[:y].to_i, date[:m], 1)
-last = Date.new(date[:y].to_i, date[:m], -1)
+params[:year] ||= Date.today.year
+params[:month] ||= (params[:year] == Date.today.year ? Date.today.month : 1)
 
-puts "      #{first.month}月 #{first.year}"
+first_day = Date.new(params[:year], params[:month], 1)
+last_day = Date.new(params[:year], params[:month], -1)
+
+puts "      #{first_day.month}月 #{first_day.year}"
 puts %w(日 月 火 水 木 金 土).join(' ') + "\n"
 
-last_day = last.day
-spaces = [' '] * first.wday
-puts (1..last_day)
+days_in_month = last_day.day
+leading_spaces = [' '] * first_day.wday
+
+puts (1..days_in_month)
   .to_a
-  .unshift(*spaces)
+  .unshift(*leading_spaces)
   .each_slice(7)
   .map {|week| week.map { |day| day.to_s.rjust(2) }.join(' ') }
   .join("\n")
