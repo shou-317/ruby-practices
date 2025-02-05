0f073beb7f99bd86579ec552a65db8ce6d8ad453
shou-317 <shou6130@gmail.com>
2025-02-05 18:08:39 +0900

    Refactor indentation and enhance calendar header formatting

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
 def parse_arguments
   option_parser = OptionParser.new
   params = {}
-  option_parser.on('-m [VAL]', Integer) {|m| params[:month] = m}
-  option_parser.on('-y [VAL]', Integer) {|m| params[:year] = m}
+  option_parser.on('-m [VAL]', Integer) { |m| params[:month] = m }
+  option_parser.on('-y [VAL]', Integer) { |m| params[:year] = m }
   option_parser.parse!(ARGV, into: params)
   params[:year] ||= Date.today.year
   params[:month] ||= (params[:year] == Date.today.year ? Date.today.month : 1)
@@ -19,17 +21,17 @@ def generate_calendar(params)
   days_in_month = last_day.day
   leading_spaces = [' '] * first_day.wday
   calendar_rows = (1..days_in_month)
-    .to_a
-    .unshift(*leading_spaces)
-    .each_slice(7)
-    .map {|week| week.map { |day| day.to_s.rjust(2) }.join(' ') }
-    .join("\n")
+                  .to_a
+                  .unshift(*leading_spaces)
+                  .each_slice(7)
+                  .map { |week| week.map { |day| day.to_s.rjust(2) }.join(' ') }
+                  .join("\n")
   display_calendar(params, calendar_rows)
 end
 
 def display_calendar(params, calendar_rows)
   puts "      #{params[:month]}月 #{params[:year]}"
-  puts %w(日 月 火 水 木 金 土).join(' ') + "\n"
+  puts "#{%w[日 月 火 水 木 金 土].join(' ')}\n"
   puts calendar_rows
 end
 
