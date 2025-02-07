004ca45971e0be566f5d6455043ce8e0cf1bbca0
shou-317 <shou6130@gmail.com>
2025-02-07 18:11:27 +0900

    refactor: Improve code readability and simplify comments
    - Updated comments for better clarity and consistency
    - Renamed variables and comments to better reflect their purpose
    - Simplified strike and spare calculation logic for improved readability

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# コマンドライン引数を配列にする
+# スコアをカンマ区切りで取得
 scores = ARGV[0].split(',')
 
-# データの整形
-# 文字列を整数に変換する
+# スコアを数値化
 shots = []
 scores.each do |score|
   if score == 'X'
@@ -16,33 +15,31 @@ scores.each do |score|
   end
 end
 
-# 整数の配列を2つずつ分ける
+# フレームを作成
 frames = []
 shots.each_slice(2) do |shot|
   frames << shot
 end
-# 0が入ると処理が複雑化するため、[10,0]を[10]にする
+# ストライクのフレームを1投にする
 frames = frames.map { |frame| frame == [10, 0] ? [10] : frame }
-# 10フレーム以降の要素を1つにまとめる
+# 10フレーム以降の投球を1つにまとめる
 frames[9..] = [frames[9..].flatten]
 
 # スコア計算
 point = 0
 point += frames.each_with_index.sum do |frame, index|
-  # 10投目はフレームの合計
-  if index == 9
-    frame.sum
-  elsif frame[0] == 10 # ストライクのとき
-    # 次のフレームがストライクかどうかで条件分岐
-    if frames[index + 1][0] == 10
-      # [[10], [10], [n(, n)]..]のとき、frames[index + 1][1]はnilのため、frames[index + 2][0]を2投目として加算する
-      frame[0] + frames[index + 1][0] + (frames[index + 1][1] || frames[index + 2][0])
-    else
-      frame[0] + frames[index + 1][0..1].sum # 次の2投を加算
-    end
+  # 10フレーム目はストライクやスペアを考慮せず合計を求める
+  next frame.sum if index == 9
+
+  if frame[0] == 10 # ストライクのとき
+    # 次の2投を加算する（次のフレームがストライクなら、さらにその次の1投も考慮）
+    next_two_shots = frames[index + 1][1] || frames[index + 2][0]
+    10 + frames[index + 1][0] + next_two_shots
   elsif frame.sum == 10 # スペアのとき
-    frame.sum + frames[index + 1][0] # 次の1投を加算
+    # 次の1投を加算
+    10 + frames[index + 1][0]
   else
+    # フレームの合計を求める
     frame.sum
   end
 end
