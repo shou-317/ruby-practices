35208fdb1ffcb149f8d4ca112685e6ca5fee4674
shou-317 <shou6130@gmail.com>
2025-02-13 17:18:52 +0900

    refactor: Remove unnecessary code and correct variable names.
    - Simplify the process of creating frames.
    - Direct assignment to point.
    - Change next_to_shots to second_bonus_ball.
    - Change 10 to frame.sum

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -17,9 +17,8 @@ end
 
 # フレームを作成
 frames = []
-shots.each_slice(2) do |shot|
-  frames << shot
-end
+frames = shots.each_slice(2).to_a
+
 # ストライクのフレームを1投にする
 frames = frames.map { |frame| frame == [10, 0] ? [10] : frame }
 # 10フレーム以降の投球を1つにまとめる
@@ -27,17 +26,17 @@ frames[9..] = [frames[9..].flatten]
 
 # スコア計算
 point = 0
-point += frames.each_with_index.sum do |frame, index|
+point = frames.each_with_index.sum do |frame, index|
   # 10フレーム目はストライクやスペアを考慮せず合計を求める
   next frame.sum if index == 9
 
   if frame[0] == 10 # ストライクのとき
     # 次の2投を加算する（次のフレームがストライクなら、さらにその次の1投も考慮）
-    next_two_shots = frames[index + 1][1] || frames[index + 2][0]
-    10 + frames[index + 1][0] + next_two_shots
+    second_bonus_ball = frames[index + 1][1] || frames[index + 2][0]
+    frame.sum + frames[index + 1][0] + second_bonus_ball
   elsif frame.sum == 10 # スペアのとき
     # 次の1投を加算
-    10 + frames[index + 1][0]
+    frame.sum + frames[index + 1][0]
   else
     # フレームの合計を求める
     frame.sum
