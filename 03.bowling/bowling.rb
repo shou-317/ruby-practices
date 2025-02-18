939020f6db829bcf85e5956e8c4d29819ae4c4a2
shou-317 <shou6130@gmail.com>
2025-02-18 18:15:13 +0900

    refactor: change to use STRIKE where appropriate
    https://github.com/hor1shota/ruby-practices/pull/3#discussion_r1952245627

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-STRIKE = 10
+STRIKE = [10].freeze
 scores = ARGV[0].split(',')
 
 shots = []
-shots = scores.flat_map { |score| score == 'X' ? [STRIKE, 0] : score.to_i }
+shots = scores.flat_map { |score| score == 'X' ? [10, 0] : score.to_i }
 
 frames = []
 frames = shots.each_slice(2).to_a
 
-frames = frames.map { |frame| frame == [STRIKE, 0] ? [STRIKE] : frame }
+frames = frames.map { |frame| frame == [10, 0] ? STRIKE : frame }
 frames[9..] = [frames[9..].flatten]
 
 point = 0
 point = frames.each_with_index.sum do |frame, index|
   next frame.sum if index == 9
 
-  if frame[0] == STRIKE
+  if frame == STRIKE
     second_bonus_ball = frames[index + 1][1] || frames[index + 2][0]
     frame.sum + frames[index + 1][0] + second_bonus_ball
-  elsif frame.sum == STRIKE
+  elsif frame.sum == 10
     frame.sum + frames[index + 1][0]
   else
     frame.sum
