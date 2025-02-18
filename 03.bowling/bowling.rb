269847125a9fd0276126efc53be79c9798ea9a9b
shou-317 <shou6130@gmail.com>
2025-02-18 18:16:33 +0900

    refactor: remove variable initialization
    https://github.com/hor1shota/ruby-practices/pull/3#discussion_r1957476341

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,16 +4,13 @@
 STRIKE = [10].freeze
 scores = ARGV[0].split(',')
 
-shots = []
 shots = scores.flat_map { |score| score == 'X' ? [10, 0] : score.to_i }
 
-frames = []
 frames = shots.each_slice(2).to_a
 
 frames = frames.map { |frame| frame == [10, 0] ? STRIKE : frame }
 frames[9..] = [frames[9..].flatten]
 
-point = 0
 point = frames.each_with_index.sum do |frame, index|
   next frame.sum if index == 9
 
