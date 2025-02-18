7b2bb639684d14432ece0e3e7a741432640581e6
shou-317 <shou6130@gmail.com>
2025-02-18 19:02:25 +0900

    refactor: split methods to increase readability

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -2,27 +2,39 @@
 # frozen_string_literal: true
 
 STRIKE = [10].freeze
-scores = ARGV[0].split(',')
 
-shots = scores.flat_map { |score| score == 'X' ? [10, 0] : score.to_i }
+def main
+  scores = ARGV[0]
+  shots = parse_score_input(scores)
+  frames = generate_frames(shots)
+  point = calculate_point(frames)
+  puts point
+end
 
-frames = shots.each_slice(2).to_a
+def parse_score_input(scores)
+  scores.split(',').flat_map { |score| score == 'X' ? [10, 0] : score.to_i }
+end
 
-frames = frames.map { |frame| frame == [10, 0] ? STRIKE : frame }
-frames[9..] = [frames[9..].flatten]
+def generate_frames(shots)
+  frames = shots.each_slice(2).map { |frame| frame == [10, 0] ? STRIKE : frame }
+  frames[9..] = [frames[9..].flatten]
+  frames
+end
 
-point = frames.each_with_index.sum do |frame, index|
-  frame_sum = frame.sum
-  next frame_sum if index == 9
+def calculate_point(frames)
+  frames.each_with_index.sum do |frame, index|
+    frame_sum = frame.sum
+    next frame_sum if index == 9
 
-  if frame == STRIKE
-    second_bonus_ball = frames[index + 1][1] || frames[index + 2][0]
-    frame_sum + frames[index + 1][0] + second_bonus_ball
-  elsif frame_sum == 10
-    frame_sum + frames[index + 1][0]
-  else
-    frame_sum
+    if frame == STRIKE
+      second_bonus_ball = frames[index + 1][1] || frames[index + 2][0]
+      frame_sum + frames[index + 1][0] + second_bonus_ball
+    elsif frame_sum == 10
+      frame_sum + frames[index + 1][0]
+    else
+      frame_sum
+    end
   end
 end
 
-puts point
+main if __FILE__ == $PROGRAM_NAME
